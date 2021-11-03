ebaddf989a59ea71d71d6d99b320828a25030c6a
osadasami <osadasami@gmail.com>
2021-11-03 22:03:18 +0500

    rust. assembly-line. done

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 - [x] [Hello World](https://exercism.org/tracks/rust/exercises/hello-world)
 - [x] [Lucian's Luscious Lasagna](https://exercism.org/tracks/rust/exercises/lucians-luscious-lasagna)
 - [x] [Semi Structured Logs](https://exercism.org/tracks/rust/exercises/semi-structured-logs)
-- [ ] [Assembly Line](https://exercism.org/tracks/rust/exercises/assembly-line)
+- [x] [Assembly Line](https://exercism.org/tracks/rust/exercises/assembly-line)
--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -3,9 +3,18 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    const CARS_PRODUCED_EACH_HOUR_AT_LOWEST_SPEED: u8 = 221;
+
+    let result = f64::from(speed) * f64::from(CARS_PRODUCED_EACH_HOUR_AT_LOWEST_SPEED);
+
+    match speed {
+        1..=4 => result,
+        5..=8 => result * 0.9,
+        9..=10 => result * 0.77,
+        _ => 0.0,
+    }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) / f64::from(60)) as u32
 }
