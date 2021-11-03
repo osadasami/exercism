8712e609d3283a3bd979e2fdbb9105ded4ee89bb
osadasami <osadasami@gmail.com>
2021-11-03 22:11:53 +0500

    rust. assembly-line. improved

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -3,18 +3,17 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    const CARS_PRODUCED_EACH_HOUR_AT_LOWEST_SPEED: u8 = 221;
+    const CARS_PRODUCED_EACH_HOUR_AT_LOWEST_SPEED: f64 = 221.0;
 
-    let result = f64::from(speed) * f64::from(CARS_PRODUCED_EACH_HOUR_AT_LOWEST_SPEED);
-
-    match speed {
-        1..=4 => result,
-        5..=8 => result * 0.9,
-        9..=10 => result * 0.77,
-        _ => 0.0,
-    }
+    CARS_PRODUCED_EACH_HOUR_AT_LOWEST_SPEED
+        * (speed as f64)
+        * match speed {
+            0..=4 => 1.0,
+            5..=8 => 0.9,
+            9..=u8::MAX => 0.77,
+        }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    (production_rate_per_hour(speed) / f64::from(60)) as u32
+    production_rate_per_hour(speed) as u32 / 60
 }
