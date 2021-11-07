8942c0da54e2da6bfd14869d66a44eebd06bde3d
osadasami <osadasami@gmail.com>
2021-11-07 15:30:34 +0500

    rust. magazine-cutout. improved

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -5,18 +5,17 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    let mut magazine_word_count: HashMap<&str, i32> = HashMap::new();
-    let mut note_word_count: HashMap<&str, i32> = HashMap::new();
+    let magazine_words_count = magazine.iter().fold(HashMap::new(), |mut words, word| {
+        *words.entry(word).or_insert(0) += 1;
+        words
+    });
 
-    for word in magazine {
-        *magazine_word_count.entry(word).or_insert(0) += 1;
-    }
+    let note_words_count = note.iter().fold(HashMap::new(), |mut words, word| {
+        *words.entry(word).or_insert(0) += 1;
+        words
+    });
 
-    for word in note {
-        *note_word_count.entry(word).or_insert(0) += 1;
-    }
-
-    note_word_count
+    note_words_count
         .iter()
-        .all(|(word, count)| *magazine_word_count.entry(word).or_insert(0) >= *count)
+        .all(&|(word, count)| magazine_words_count.get(word).unwrap_or(&0) >= count)
 }
