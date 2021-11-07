cc5d94e8415e0fd351d9fccf694b2542f841ab61
osadasami <osadasami@gmail.com>
2021-11-07 15:16:51 +0500

    rust. magazine-cutout. done

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -5,5 +5,18 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut magazine_word_count: HashMap<&str, i32> = HashMap::new();
+    let mut note_word_count: HashMap<&str, i32> = HashMap::new();
+
+    for word in magazine {
+        *magazine_word_count.entry(word).or_insert(0) += 1;
+    }
+
+    for word in note {
+        *note_word_count.entry(word).or_insert(0) += 1;
+    }
+
+    note_word_count
+        .iter()
+        .all(|(word, count)| *magazine_word_count.entry(word).or_insert(0) >= *count)
 }
