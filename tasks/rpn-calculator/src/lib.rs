70a0d5723e79979c3f073091b5b198d011d02857
osadasami <osadasami@gmail.com>
2021-11-08 17:07:14 +0500

    rust. rpn-calculator. done

--- tasks/rpn-calculator/src/lib.rs
+++ tasks/rpn-calculator/src/lib.rs
@@ -8,5 +8,31 @@ pub enum CalculatorInput {
 }
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    None
+    let mut stack = Vec::new();
+
+    for input in inputs {
+        match input {
+            CalculatorInput::Value(n) => stack.push(*n),
+            _ => {
+                let n2 = stack.pop()?;
+                let n1 = stack.pop()?;
+
+                let result = match input {
+                    CalculatorInput::Add => n1 + n2,
+                    CalculatorInput::Subtract => n1 - n2,
+                    CalculatorInput::Multiply => n1 * n2,
+                    CalculatorInput::Divide => n1 / n2,
+                    _ => 0,
+                };
+
+                stack.push(result);
+            }
+        }
+    }
+
+    if stack.len() > 1 || inputs.is_empty() {
+        None
+    } else {
+        Some(stack[0])
+    }
 }
