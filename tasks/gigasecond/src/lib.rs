eff6d27d5d79a622a272494378b9beb6c1bb4f6d
osadasami <osadasami@gmail.com>
2021-11-10 14:48:06 +0500

    rust. gigasecond. improved

--- tasks/gigasecond/src/lib.rs
+++ tasks/gigasecond/src/lib.rs
@@ -1,11 +1,7 @@
-use time::{OffsetDateTime, PrimitiveDateTime as DateTime};
+use time::Duration;
+use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    const GIGASECOND: i64 = 1_000_000_000;
-    let after_gigasecond =
-        OffsetDateTime::from_unix_timestamp(start.assume_utc().unix_timestamp() + GIGASECOND)
-            .unwrap();
-
-    DateTime::new(after_gigasecond.date(), after_gigasecond.time())
+    start + Duration::seconds(1_000_000_000)
 }
