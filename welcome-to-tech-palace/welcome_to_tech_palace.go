bc0f2090a20a50cbe9df300c06bbf08eda169238
osadasami <osadasami@gmail.com>
2021-11-11 21:49:48 +0500

    golang. welcome-to-tech-palace. done

--- welcome-to-tech-palace/welcome_to_tech_palace.go
+++ welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,25 @@
 package techpalace
 
+import (
+	"fmt"
+	"strings"
+)
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	return fmt.Sprintf("Welcome to the Tech Palace, %s", strings.ToUpper(customer))
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	return fmt.Sprintf("%s\n%s\n%s",
+		strings.Repeat("*", numStarsPerLine),
+		welcomeMsg,
+		strings.Repeat("*", numStarsPerLine),
+	)
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	return strings.TrimSpace(strings.ReplaceAll(oldMsg, "*", ""))
 }
