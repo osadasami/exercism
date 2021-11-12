3c453978f0cbff881e4cd4fed67c7f8c85728f11
osadasami <osadasami@gmail.com>
2021-11-12 11:25:59 +0500

    rust. beer-song. done

--- tasks/beer-song/src/lib.rs
+++ tasks/beer-song/src/lib.rs
@@ -1,7 +1,23 @@
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {}", n)
+    match n {
+        0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"),
+        n => format!(
+            "{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle{} of beer on the wall.\n",
+            n,
+            n,
+            n-1,
+            if n-1 > 1 {"s"} else {""}
+        ),
+    }
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    let mut song: Vec<String> = Vec::new();
+
+    for n in end..=start {
+        song.push(verse(n))
+    }
+
+    song.into_iter().rev().collect::<Vec<String>>().join("\n")
 }
