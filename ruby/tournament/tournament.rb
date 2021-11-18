7944cb5a7cf016ceb2dde66475d97e0234e870a1
osadasami <osadasami@gmail.com>
2021-11-18 15:44:37 +0500

    ruby. tournament. done

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,7 +1,53 @@
-=begin
-Write your code for the 'Tournament' exercise in this file. Make the tests in
-`tournament_test.rb` pass.
+class Tournament
+	def self.tally(input)
+		rows = input.split("\n")
+		teams = {}
 
-To get started with TDD, see the `README.md` file in your
-`ruby/tournament` directory.
-=end
+		rows.each do |row|
+			team_a, team_b, result = row.split(';')
+			teams[team_a] = {wins: 0, drawns: 0, losses: 0, matches_player: 0, points: 0}
+			teams[team_b] = {wins: 0, drawns: 0, losses: 0, matches_player: 0, points: 0}
+		end
+
+		rows.each do |row|
+			team_a, team_b, result = row.split(';')
+
+			if result == 'win'
+				teams[team_a][:wins] += 1
+				teams[team_a][:points] += 3
+				teams[team_b][:losses] += 1
+			end
+
+			if result == 'loss'
+				teams[team_a][:losses] += 1
+				teams[team_b][:wins] += 1
+				teams[team_b][:points] += 3
+			end
+
+			if result == 'draw'
+				teams[team_a][:drawns] += 1
+				teams[team_a][:points] += 1
+				teams[team_b][:drawns] += 1
+				teams[team_b][:points] += 1
+			end
+
+			teams[team_a][:matches_player] += 1
+			teams[team_b][:matches_player] += 1
+		end
+
+		header = 'Team                           | MP |  W |  D |  L |  P'
+    
+    body = teams
+    	.sort_by { |team, stats| team }
+    	.sort_by { |team, stats| -stats[:points] }
+  		.map { |team, stats| "#{team.ljust(30)} |  #{stats[:matches_player]} |  #{stats[:wins]} |  #{stats[:drawns]} |  #{stats[:losses]} |  #{stats[:points]}" }
+  		.join("\n")
+
+		output = <<~TALLY
+			#{header}
+			#{body}
+    TALLY
+
+    output.strip.concat("\n")
+	end
+end
