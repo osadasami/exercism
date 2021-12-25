c8437e4f2a158d9236163f76610accd19306ed02
osadasami <osadasami@gmail.com>
2021-12-25 16:46:37 +0500

    rust. armstrong-numbers. improved

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,15 +1,12 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    if num == 0 {
-        return true;
-    }
-
     let num_str = num.to_string();
-    let num_length = num_str.len();
-    let result = num_str
-        .split("")
-        .filter(|s| !s.is_empty())
-        .map(|n| n.parse::<u32>().unwrap())
-        .fold(0, |acc, n| acc + u32::pow(n, num_length as u32));
+    let num_len = num_str.len() as u32;
 
-    result == num
+    let sum = num_str
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .map(|d| d.pow(num_len))
+        .sum();
+
+    num == sum
 }
